7719f95e18b95ec279ee15b0a86abf7a9ba266ad
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2016-11-30 14:39:34 -0500

    Corrected spelling mistake causing error

--- server/service.js
+++ server/service.js
@@ -18,7 +18,7 @@ service.get('/service/:location', (req, res, next) => {
             console.log(err);
             return res.sendStatus(500);
         }
-        const location = response.body.result[0].geometry.location;
+        const location = response.body.results[0].geometry.location;
         const timestamp = +moment().format('X');
 
         request.get('https://maps.googleapis.com/maps/api/timezone/json?location='+ location.lat + ',' + location.lng + '&timestamp='+ timestamp +'&key=AIzaSyCPcvT0EePNTelpu3oTH-VxlM9PxyMu7qA', (err, response) => {
